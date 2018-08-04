854b3ff4eb213a7d5fd111c83cba354243feebb9
Mystic5523 <Mystic5523@hotmail.com>
2018-08-04 11:40:52 -0500

    getting there

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
+var path = require("path");
+
 
 // @Boilerplate
 const app = express()
@@ -19,58 +21,87 @@ app.use(bodyParser.json())
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }))
 
 // @Routes
-app.get('/:endpoint?', function (req, res) {
-  switch (req.params.endpoint) {
-    case undefined:
-    case '/':
-      sendFile(res, 'index.html')
-      break
-    case 'reserve':
-      sendFile(res, 'reserve.html')
-      break
-    case 'tables':
-      sendFile(res, 'tables.html')
-      break
-    default:
-      sendFile(res, '404.html')
-      break
-  }
+// // app.get('/:endpoint?', function(req, res) {
+//     switch (req.params.endpoint) {
+//         case undefined:
+//             break
+//         case '/':
+//             res.sendFile(path.join(__dirname, "index.html"));
+//             break
+//         case 'reserve':
+//             res.sendFile(res, 'reserve.html')
+//             break
+//         case 'tables':
+//             res.sendFile(res, 'tables.html')
+//             break
+//         default:
+//             res.sendFile(res, '404.html')
+//             break
+//     }
+// })
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/reserve", function(req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+// Displays all characters
+app.get("/api/characters", function(req, res) {
+    return res.json(characters);
+});
+
+// Displays a single character, or returns false
+app.get("/api/characters/:character", function(req, res) {
+    var chosen = req.params.character;
+
+    console.log(chosen);
+
+    for (var i = 0; i < characters.length; i++) {
+        if (chosen === characters[i].routeName) {
+            return res.json(characters[i]);
+        }
+    }
+
+    return res.json(false);
+});
+
+app.get('/api/:endpoint?', function(req, res) {
+    switch (req.params.endpoint) {
+        case 'tables':
+            res.json(tables)
+            break
+        case 'waitlist':
+            res.json(tables.slice(5))
+            break
+        default:
+            res.status(404).json({ error: 'Not Found' })
+            break
+    }
 })
 
-app.get('/api/:endpoint?', function (req, res) {
-  switch (req.params.endpoint) {
-    case 'tables':
-      res.json(tables)
-      break
-    case 'waitlist':
-      res.json(tables.slice(5))
-      break
-    default:
-      res.status(404).json({error: 'Not Found'})
-      break
-  }
-})
+app.post('/tables', function(req, res) {
+    const table = req.body
 
-app.post('/tables', function (req, res) {
-  const table = req.body
+    let response
+    if (!isValid(table)) {
+        res.status(400)
+        response = { error: 'Malformed input. Check your submission and try again.' }
+    } else {
+        res.status(201)
+        tables.push(table)
+    }
 
-  let response
-  if (!isValid(table)) {
-    res.status(400)
-    response = {error: 'Malformed input. Check your submission and try again.'}
-  } else {
-    res.status(201)
-    tables.push(table)
-  }
-
-  res.json(response)
+    res.json(response)
 })
 
 // @Start
-app.listen(PORT, function (err) {
-  if (err) {
-    return console.error(err)
-  }
+app.listen(PORT, function(err) {
+    if (err) {
+        return console.error(err)
+    }
 
-  console.log(`Listening on port ${PORT}.`)
+    console.log(`Listening on port ${PORT}.`)
 })
